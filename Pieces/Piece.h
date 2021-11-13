4c06a810a7778f6ff93af66651ce6823aa9be4a1
Yigit Sen <ysen@connect.ust.hk>
2021-11-13 21:53:16 +0800

    edits to piece.h

--- Pieces/Piece.h
+++ Pieces/Piece.h
@@ -4,9 +4,37 @@
 
 #ifndef INC_4PCHESS_PIECE_H
 #define INC_4PCHESS_PIECE_H
-
+#include <string>
+class Player;
+class Game;
 
 class Piece {
+    public:
+        enum class Type {
+            BISHOP = 1,
+            KING = 2,
+            KNIGHT = 3,
+            PAWN = 4,
+            QUEEN = 5,
+            ROOK = 6
+        };
+
+        virtual ~Piece() = default;
+
+        virtual Piece::Type get_type() const = 0;
+
+        virtual int get_point() const = 0;
+
+    protected:
+        explicit Piece(Player &player, Game &game);
+
+        Player &player;
+        Game &game
+
+    private:
+
+
+    };
 
 };
 

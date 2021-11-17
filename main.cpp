81089d6042458618f21ea40295a4e6d1d92977ac
Yigit Sen <ysen@connect.ust.hk>
2021-11-17 23:28:33 +0800

    Everything except King

--- main.cpp
+++ main.cpp
@@ -7,11 +7,116 @@ int main()
 {
     Game* temp = new Game();
     temp->print_board();
+    std::cout<<std::endl;
     temp->delete_piece(12, 10);
     temp->print_board();
+    std::cout<<std::endl;
+
     temp->get_piece(13,10)->list_possible_moves();
+    std::cout<<std::endl;
+
     temp->movePiece(13, 10, 7, 7);
     temp->print_board();
+    std::cout<<std::endl;
+
     temp->get_piece(7,7)->list_possible_moves();
+
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+
+
+    Game* temp2 = new Game();
+    temp2->print_board();
+    std::cout<<std::endl;
+
+    temp2->delete_piece(12, 6);
+    temp2->delete_piece(12, 7);
+    temp2->delete_piece(12, 8);
+
+    temp2->print_board();
+    std::cout<<std::endl;
+
+    temp2->get_piece(13,7)->list_possible_moves();
+    std::cout<<std::endl;
+
+
+
+
+    temp2->movePiece(13, 7, 7, 7);
+    temp2->print_board();
+    std::cout<<std::endl;
+
+    temp2->get_piece(7,7)->list_possible_moves();
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+
+    Game* temp3 = new Game();
+    temp3->print_board();
+    std::cout<<std::endl;
+
+    temp3->get_piece(13,9)->list_possible_moves();
+    std::cout<<std::endl;
+
+    temp3->movePiece(13, 9, 7, 7);
+    temp3->print_board();
+    std::cout<<std::endl;
+
+    temp3->get_piece(7,7)->list_possible_moves();
+
+
+    Game* temp4 = new Game();
+    temp4->print_board();
+    std::cout<<std::endl;
+
+    temp4->delete_piece(12, 9);
+    temp4->delete_piece(12, 7);
+    temp4->delete_piece(12, 8);
+
+    temp4->print_board();
+    std::cout<<std::endl;
+
+    temp4->get_piece(13,8)->list_possible_moves();
+    std::cout<<std::endl;
+
+
+
+
+    temp4->movePiece(13, 8, 7, 7);
+    temp4->print_board();
+    std::cout<<std::endl;
+
+    temp4->get_piece(7,7)->list_possible_moves();
+
+
+    Game* temp5 = new Game();
+    temp5->print_board();
+    std::cout<<std::endl;
+    temp5->get_piece(12,3)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(12,4)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(10,1)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(9,1)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(1,3)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(1,4)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(4,12)->list_possible_moves();
+    std::cout<<std::endl;
+    temp5->get_piece(5,12)->list_possible_moves();
+    std::cout<<std::endl;
+
+
 
 }
--- pieces/Bishop.cpp
+++ pieces/Bishop.cpp
@@ -4,8 +4,8 @@
 
 #include "Bishop.h"
 #include "Piece.h"
-
-
+#include "iostream"
+#include "../Game.h"
 
 Bishop::Bishop(Player& player, Game& game, int x, int y): Piece(player, game, x, y) {}
 
@@ -18,13 +18,127 @@ int Bishop::get_point() const{
 }
 
 void Bishop::list_possible_moves(){
-    //Check for +Y-dir
-    int xpos = 0;
+
+    //Check for +Y (up)
+    int temp[14][14] = {0};
+    temp[x][y] = 1;
+
+    int xpos = x+1;
+    int ypos = y+1;
     while(true)
     {
+        if(game.in_boundaries(xpos, ypos))
+        {
+            if(game.get_piece(xpos, ypos) == nullptr)
+            {
+                temp[xpos][ypos] = 2;
+            }
+            else
+            {
+                if(game.get_piece(xpos, ypos)->which_player() != this->which_player())
+                {
+                    temp[xpos][ypos] = 2;
+                }
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+        ypos++;
+        xpos++;
+    }
 
+    xpos = x-1;
+    ypos = y+1;
+    while(true)
+    {
+        if(game.in_boundaries(xpos, ypos))
+        {
+            if(game.get_piece(xpos, ypos) == nullptr)
+            {
+                temp[xpos][ypos] = 2;
+            }
+            else
+            {
+                if(game.get_piece(xpos, ypos)->which_player() != this->which_player())
+                {
+                    temp[xpos][ypos] = 2;
+                }
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+        ypos++;
+        xpos--;
+    }
+    xpos = x-1;
+    ypos = y-1;
+    while(true)
+    {
+        if(game.in_boundaries(xpos, ypos))
+        {
+            if(game.get_piece(xpos, ypos) == nullptr)
+            {
+                temp[xpos][ypos] = 2;
+            }
+            else
+            {
+                if(game.get_piece(xpos, ypos)->which_player() != this->which_player())
+                {
+                    temp[xpos][ypos] = 2;
+                }
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+        ypos--;
+        xpos--;
+    }
+
+    xpos = x+1;
+    ypos = y-1;
+    while(true)
+    {
+        if(game.in_boundaries(xpos, ypos))
+        {
+            if(game.get_piece(xpos, ypos) == nullptr)
+            {
+                temp[xpos][ypos] = 2;
+            }
+            else
+            {
+                if(game.get_piece(xpos, ypos)->which_player() != this->which_player())
+                {
+                    temp[xpos][ypos] = 2;
+                }
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+        ypos--;
+        xpos++;
+    }
+
+
+
+
+    for (int i = 0; i < 14; ++i) {
+        for (int j = 0; j < 14; ++j) {
+            std::cout<<temp[i][j] << " ";
+        }
+        std::cout<<std::endl;
 
     }
-    //Do nothing
 }
 

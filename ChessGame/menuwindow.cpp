cafbb09dff767924599ea28c74b974bfa78dc206
godly-recluse <63873623+godly-recluse@users.noreply.github.com>
2021-11-27 21:53:48 +0800

    Update menuwindow.cpp

--- ChessGame/menuwindow.cpp
+++ ChessGame/menuwindow.cpp
@@ -21,6 +21,7 @@ void MenuWindow::on_start_game_clicked()
     std::cout<<ui->player_count->currentText().toInt()<<std::endl;
     MainWindow *m = new MainWindow(4-ui->player_count->currentText().toInt());
     m->setAttribute(Qt::WA_DeleteOnClose);
+    m->setStyleSheet("QWidget{font:Arial}");
     m->show();
 
     close();
